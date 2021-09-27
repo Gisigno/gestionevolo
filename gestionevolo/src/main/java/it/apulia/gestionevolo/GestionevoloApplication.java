2ae43e47bcb968bc40b6bc5afc83ba73360a7a57
GiacomoSignorile <gsignorile@studenti.apuliadigitalmaker.it>
2021-09-27 16:47:59 +0200

    ...

--- gestionevolo/src/main/java/it/apulia/gestionevolo/GestionevoloApplication.java
+++ gestionevolo/src/main/java/it/apulia/gestionevolo/GestionevoloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestionevoloApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GestionevoloApplication.class, args);
 	}
 
